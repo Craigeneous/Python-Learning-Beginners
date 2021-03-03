46b8ecb0e11d03a99829f40379cd576640f43545
Craigeneous <78416905+Craigeneous@users.noreply.github.com>
2021-03-03 12:51:35 +0100

    3/3/21

---
+++ CLASSES and OBJECTS.py
@@ -0,0 +1,11 @@
+# Classes are groups of data and functions that describe a particular template. Objects are individual instances of that
+# class template being used. Classes can be used to describe almost anything:-
+# A class for dogs might include breed, age, colouration, a true/false for being chipped.
+# A class for merging documents might include the first document, the second, the name of the new merged document.
+# A class for questions; question and answer, perhaps a weighting for each answer.
+
+quiz_questions = [
+    "What colour are apples?\n(a) RED\n(b) BLUE\n(c) YELLOW\n\n",
+    "What colour are bananas\n(a) ORANGE\n(b) PURPLE\n(c) YELLOW\n\n",
+    "What colour are blueberries\n(a) BLUE\n(b) GREEN\n(c) BROWN\n\n"
+]
---
+++ test.py
@@ -0,0 +1,7 @@
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+for num_in_x in x:
+    for num_in_y in y:
+        print(num_in_y*num_in_x)
+
+
